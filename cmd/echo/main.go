767ef7d98eb56119f82a43a355c512d6d8767ec9
Simon Kwame <simonkwame@aol.com>
2023-12-19 22:34:06 -0500

    Initial commit

---
+++ cmd/echo/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"strings"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/hashicorp/go-plugin"
+	"github.com/kubeshop/botkube/pkg/api"
+	"github.com/kubeshop/botkube/pkg/api/executor"
+	"github.com/kubeshop/botkube/pkg/pluginx"
+)
+
+const description = "Echo sends back the command that was specified."
+
+// version is set via ldflags by GoReleaser.
+var version = "dev"
+
+// Config holds the executor configuration.
+type Config struct {
+	TransformResponseToUpperCase *bool `yaml:"transformResponseToUpperCase,omitempty"`
+}
+
+// EchoExecutor implements the Botkube executor plugin interface.
+type EchoExecutor struct{}
+
+// Metadata returns details about the Echo plugin.
+func (EchoExecutor) Metadata(context.Context) (api.MetadataOutput, error) {
+	return api.MetadataOutput{
+		Version:     version,
+		Description: description,
+		JSONSchema: api.JSONSchema{
+			Value: heredoc.Docf(`{
+			  "$schema": "http://json-schema.org/draft-04/schema#",
+			  "title": "echo",
+			  "description": "%s",
+			  "type": "object",
+			  "properties": {
+			    "formatOptions": {
+			      "description": "Options to format echoed string",
+			      "type": "array",
+			      "items": {
+			        "type": "string",
+			        "enum": [ "bold", "italic" ]
+			      }
+			    }
+			  },
+			  "additionalProperties": false
+			}`, description),
+		},
+	}, nil
+}
+
+// Execute returns a given command as a response.
+//
+//nolint:gocritic  //hugeParam: in is heavy (80 bytes); consider passing it by pointer
+func (EchoExecutor) Execute(_ context.Context, in executor.ExecuteInput) (executor.ExecuteOutput, error) {
+	var cfg Config
+	err := pluginx.MergeExecutorConfigs(in.Configs, &cfg)
+	if err != nil {
+		return executor.ExecuteOutput{}, err
+	}
+
+	response := in.Command
+	if cfg.TransformResponseToUpperCase != nil && *cfg.TransformResponseToUpperCase {
+		response = strings.ToUpper(response)
+	}
+
+	return executor.ExecuteOutput{
+		Message: api.NewCodeBlockMessage(response, true),
+	}, nil
+}
+
+func (EchoExecutor) Help(context.Context) (api.Message, error) {
+	btnBuilder := api.NewMessageButtonBuilder()
+	return api.Message{
+		Sections: []api.Section{
+			{
+				Base: api.Base{
+					Header:      "Run `echo` commands",
+					Description: description,
+				},
+				Buttons: []api.Button{
+					btnBuilder.ForCommandWithDescCmd("Run", "echo 'hello world'"),
+				},
+			},
+		},
+	}, nil
+}
+
+func main() {
+	executor.Serve(map[string]plugin.Plugin{
+		"echo": &executor.Plugin{
+			Executor: &EchoExecutor{},
+		},
+	})
+}
---
+++ cmd/ticker/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/kubeshop/botkube/pkg/api"
+	"github.com/kubeshop/botkube/pkg/api/source"
+	"github.com/kubeshop/botkube/pkg/pluginx"
+)
+
+var _ source.Source = (*Ticker)(nil)
+
+// version is set via ldflags by GoReleaser.
+var version = "dev"
+
+// Config holds the source configuration.
+type Config struct {
+	Interval time.Duration `yaml:"interval,omitempty"`
+}
+
+// Ticker implements the Botkube executor plugin interface.
+type Ticker struct {
+	// specify that the source doesn't handle external requests
+	source.HandleExternalRequestUnimplemented
+}
+
+// Metadata returns details about the Ticker plugin.
+func (Ticker) Metadata(_ context.Context) (api.MetadataOutput, error) {
+	return api.MetadataOutput{
+		Version:     version,
+		Description: "Emits an event at a specified interval",
+	}, nil
+}
+
+// Stream sends an event after configured time duration.
+//
+//nolint:gocritic // hugeParam: in is heavy (120 bytes); consider passing it by pointer
+func (Ticker) Stream(ctx context.Context, in source.StreamInput) (source.StreamOutput, error) {
+	cfg, err := mergeConfigs(in.Configs)
+	if err != nil {
+		return source.StreamOutput{}, err
+	}
+
+	ticker := time.NewTicker(cfg.Interval)
+	out := source.StreamOutput{
+		Event: make(chan source.Event),
+	}
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				ticker.Stop()
+			case <-ticker.C:
+				out.Event <- source.Event{
+					Message: api.NewPlaintextMessage("Ticker Event", true),
+				}
+			}
+		}
+	}()
+
+	return out, nil
+}
+
+func main() {
+	source.Serve(map[string]plugin.Plugin{
+		"ticker": &source.Plugin{
+			Source: &Ticker{},
+		},
+	})
+}
+
+func mergeConfigs(configs []*source.Config) (Config, error) {
+	defaults := Config{
+		Interval: time.Minute,
+	}
+
+	var cfg Config
+	err := pluginx.MergeSourceConfigsWithDefaults(defaults, configs, &cfg)
+	if err != nil {
+		return Config{}, fmt.Errorf("while parsing input configuration: %w", err)
+	}
+	return cfg, nil
+}
